61402976655e75c844310e6a04ebe63d5b0a0180
takao0911 <th.mfd1012@gmail.com>
2025-01-12 21:04:57 +0900

    宿題の提出

---
+++ syukudai/1.06.js
@@ -0,0 +1,18 @@
+//======== 前略 ========
+
+// add-btnというidを持つHTML要素を取得し、定数に代入する
+const addBtn = document.getElementById('add-btn');
+// parent-listというidを持つHTML要素を取得し、定数に代入する
+const parentList = document.getElementById('parent-list');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+addBtn.addEventListener('click', () => {
+  // li要素を新しく作成する
+  const childList = document.createElement('li');
+
+  // 作成したli要素に「これはリスト要素です」というテキストを追加する
+  childList.textContent = 'こちらが表示されるコンテンツです';
+
+  // 作成したli要素をul要素の子要素として末尾に追加する
+  parentList.appendChild(childList);
+});
